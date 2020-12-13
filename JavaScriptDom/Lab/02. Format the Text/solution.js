1a01d8c25d5d22fe6ad50c5ae88fc691f77fe66c
TodorovStanimir <52385215+TodorovStanimir@users.noreply.github.com>
2020-12-13 16:22:18 +0200

    feat: solved second task

--- JavaScriptDom/Lab/02. Format the Text/solution.js
+++ JavaScriptDom/Lab/02. Format the Text/solution.js
@@ -1,3 +1,29 @@
 function solve() {
-  //TODO...
+  const inputEl = document.getElementById("input");
+  const outputEl = document.getElementById("output");
+
+  if (inputEl === null || outputEl === null) {
+    throw new Error("Something happened, please reload the page");
+  }
+
+  const inputText = inputEl.innerText;
+
+  inputText
+    .split(".")
+    .filter((sentence) => sentence)
+    .reduce(
+      (acum, sentence) => {
+        acum[acum.length - 1].length !== 3
+          ? acum[acum.length - 1].push(sentence + ".")
+          : acum.push([sentence + "."]);
+
+        return acum;
+      },
+      [[]]
+    )
+    .map((arr) => {
+      const newPEl = document.createElement("p");
+      newPEl.innerText = arr.join("");
+      outputEl.appendChild(newPEl);
+    });
 }
