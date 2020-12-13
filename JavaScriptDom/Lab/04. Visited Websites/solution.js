4d2ff1e1d4f5053261e0f11b0fc243352864b1c3
TodorovStanimir <52385215+TodorovStanimir@users.noreply.github.com>
2020-12-14 00:03:44 +0200

    feat: solved fourth task

--- JavaScriptDom/Lab/04. Visited Websites/solution.js
+++ JavaScriptDom/Lab/04. Visited Websites/solution.js
@@ -1,4 +1,24 @@
 function solve() {
+  const state = {};
 
-  //TODO...
+  const findKey = (el) => el.querySelector("a").textContent.trim();
+  const template = (x) => `visited ${x} times`;
+
+  const elements = Array.from(document.querySelectorAll(".link-1"));
+
+  elements.map((el) => {
+    const key = findKey(el);
+    const value = el.querySelector("p").textContent.match(/\d+/)[0];
+    Object.defineProperty(state, `${key}`, { value: +value, writable: true });
+  });
+
+  document.addEventListener("click", (event) => {
+    const clickedEl = event.target.parentNode.parentNode;
+
+    if (clickedEl.classList.contains("link-1")) {
+      clickedEl.querySelector("p").textContent = template(
+        ++state[findKey(clickedEl)]
+      );
+    }
+  });
 }
