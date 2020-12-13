20fe3501d40d5a07fb185ab60ae9334d9174e07e
TodorovStanimir <52385215+TodorovStanimir@users.noreply.github.com>
2020-12-13 13:20:56 +0200

    feat: solved first task

--- JavaScriptDom/Lab/01. Articles List/solution.js
+++ JavaScriptDom/Lab/01. Articles List/solution.js
@@ -1,4 +1,28 @@
 function createArticle() {
-	
-	//TODO...
+  const titleEl = document.getElementById("createTitle");
+  const contentEl = document.getElementById("createContent");
+  const articlesEl = document.getElementById("articles");
+
+  if (titleEl === null || contentEl === null || articlesEl === null) {
+    throw new Error("Something happened, please reload the page");
+  }
+
+  const titleArticle = titleEl.value;
+  const contentArticle = contentEl.value;
+
+  if (titleArticle && contentArticle) {
+		const articleEl = document.createElement('article');
+		const hEl = document.createElement('h3');
+		const pEl = document.createElement('p');
+
+		hEl.innerText = titleArticle;
+		pEl.innerHTML = contentArticle;
+
+		articleEl.appendChild(hEl);
+		articleEl.appendChild(pEl);
+		articlesEl.appendChild(articleEl);
+
+    titleEl.value = "";
+		contentEl.value = "";
+  }
 }
