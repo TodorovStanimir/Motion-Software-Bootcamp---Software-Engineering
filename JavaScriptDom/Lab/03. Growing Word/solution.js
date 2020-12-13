3f026a2ad1d1529db02c9023d16032b35dbde731
TodorovStanimir <52385215+TodorovStanimir@users.noreply.github.com>
2020-12-13 20:13:22 +0200

    feat: solved third task

--- JavaScriptDom/Lab/03. Growing Word/solution.js
+++ JavaScriptDom/Lab/03. Growing Word/solution.js
@@ -1,4 +1,33 @@
 function growingWord() {
+  const exerciseEl = document.querySelector("#exercise");
+  const holdColorsEl = document.querySelector("#colors");
 
-  //TODO...
+  if (exerciseEl === null || holdColorsEl === null) {
+    throw new Error("Something happened, please reload the page");
+  }
+
+  const growEl = exerciseEl.querySelector("p");
+
+  if (growEl === null) {
+    throw new Error("Grow element missing. Please reload the page");
+  }
+
+  const currentFontSize = growEl.style.fontSize
+    ? growEl.style.fontSize.match(/\d+/)[0]
+    : 0;
+  const currentColor = growEl.style.color;
+
+  const colors = Array.from(holdColorsEl.querySelectorAll("div")).map((el) =>
+    window.getComputedStyle(el, null).getPropertyValue("background-color")
+  );
+
+  const colorIndex = colors.findIndex((color) => color === currentColor);
+
+  const newColorIndex =
+    colorIndex === -1 || colorIndex === colors.length - 1 ? 0 : colorIndex + 1;
+
+  const newSize = currentFontSize === 0 ? "2px" : 2 * currentFontSize + "px";
+
+  growEl.style.color = colors[newColorIndex];
+  growEl.style.fontSize = newSize;
 }
