b5c218ceb4c6db2b2903b632f8b4dcdb5a1bacd5
TodorovStanimir <52385215+TodorovStanimir@users.noreply.github.com>
2020-12-14 23:18:19 +0200

    feat: solved Numpad-Calculator

--- JavaScriptDom/Exercises/04. Numpad-Calculator/solution.js
+++ JavaScriptDom/Exercises/04. Numpad-Calculator/solution.js
@@ -1,4 +1,45 @@
 function solve() {
+  const expressionEl = document.querySelector("#expressionOutput");
+  const outputEl = document.querySelector("#resultOutput");
+  const numberRegex = /(\d*\.?\d+)/gi;
+  const arithmeticOperators = /\+|-|x|\//gi;
 
-    //TODO...
+  const mapper = {
+    "-": (x, y) => x - y,
+    "+": (x, y) => x + y,
+    "/": (x, y) => x / y,
+    x: (x, y) => x * y,
+  };
+
+  const specialKeys = {
+    C: () => {
+      expressionEl.textContent = "";
+      outputEl.textContent = "";
+    },
+    "=": () => {
+      const numbers = expressionEl.textContent.match(numberRegex).map(Number);
+      const operators = expressionEl.textContent.match(arithmeticOperators);
+
+      if (numbers.length === 1 || numbers.length <= operators.length) {
+        return (outputEl.textContent = "NaN");
+      }
+
+      outputEl.textContent = numbers.reduce((acc, el, ind) => {
+        return ind === 0 ? el : mapper[operators[ind - 1]](acc, el);
+      }, 0);
+    },
+  };
+
+  document.addEventListener("click", (e) => {
+    if (e.target.nodeName !== "BUTTON") {
+      return;
+    }
+
+    const keyValue = e.target.textContent;
+
+    typeof specialKeys[keyValue] === "function"
+      ? specialKeys[keyValue]()
+      : (expressionEl.textContent +=
+          +keyValue >= 0 || keyValue === "." ? `${keyValue}` : ` ${keyValue} `);
+  });
 }
