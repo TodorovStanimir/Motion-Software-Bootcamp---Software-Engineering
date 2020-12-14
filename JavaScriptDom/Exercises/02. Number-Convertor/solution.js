4a91f33f0ae05428fe32e1b1b0c3e6a3846843e3
TodorovStanimir <52385215+TodorovStanimir@users.noreply.github.com>
2020-12-14 19:04:00 +0200

    feat: solved task Number Convertor

--- JavaScriptDom/Exercises/02. Number-Convertor/solution.js
+++ JavaScriptDom/Exercises/02. Number-Convertor/solution.js
@@ -1,4 +1,35 @@
 function solve() {
+  const inputEl = document.querySelector("#input");
+  const toSelectEl = document.querySelector("#selectMenuTo");
+  const outputEl = document.querySelector("#result");
+  const ctaBtn = document.querySelector("button");
 
-    //TODO...
+  if (
+    inputEl === null ||
+    toSelectEl === null ||
+    outputEl === null ||
+    ctaBtn === null
+  ) {
+    throw new Error("Something happens please reload the page!");
+  }
+
+  ["Binary", "Hexadecimal"].map((str) => {
+    const optionEl = document.createElement("option");
+    optionEl.value = str.toLowerCase();
+    optionEl.innerText = str;
+    toSelectEl.appendChild(optionEl);
+  });
+
+  ctaBtn.addEventListener("click", () => {
+    const inputAmount = Number(inputEl.value);
+
+    if (inputAmount && toSelectEl.value) {
+      const outputAmount = {
+        hexadecimal: (x) => x.toString(16),
+        binary: (x) => x.toString(2),
+      }[toSelectEl.value](inputAmount);
+
+      outputEl.value = outputAmount;
+    }
+  });
 }
