ae8d20fcfde94787a9e0da32fbb1f41d7218da16
TodorovStanimir <52385215+TodorovStanimir@users.noreply.github.com>
2020-12-14 14:16:39 +0200

    feat: solved task chat room

--- JavaScriptDom/Exercises/01. Chat-Room/solution.js
+++ JavaScriptDom/Exercises/01. Chat-Room/solution.js
@@ -1,5 +1,18 @@
 function solve() {
-   //TODO...
+  const inputEl = document.querySelector("#chat_input");
+  const chatMessageEl = document.querySelector("#chat_messages");
+  const ctaEl = document.querySelector("#send");
+
+  if (inputEl === null || chatMessageEl === null || ctaEl === null) {
+    throw new Error("Something happens please reload the page!");
+  }
+
+  ctaEl.addEventListener("click", (e) => {
+    const message = inputEl.value;
+    const newDivEl = document.createElement("div");
+    newDivEl.classList.add("message", "my-message");
+    newDivEl.innerText = message;
+    chatMessageEl.appendChild(newDivEl);
+    inputEl.value = "";
+  });
 }
-
-
