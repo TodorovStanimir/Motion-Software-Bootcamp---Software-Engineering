882d3c474a178c97f2bd32f7b6f59171842e3e3a
TodorovStanimir <52385215+TodorovStanimir@users.noreply.github.com>
2020-12-15 07:56:04 +0200

    feat: solved Order-the-Names

--- JavaScriptDom/Exercises/05. Order-the-Names/solution.js
+++ JavaScriptDom/Exercises/05. Order-the-Names/solution.js
@@ -1,4 +1,34 @@
 function solve() {
+  const actions = {
+    button: () => {
+      const liElements = olEl.querySelectorAll("li");
 
-    //TODO...
+      const name = inputEl.value
+        .split("")
+        .map((el, ind) => (ind === 0 ? el.toUpperCase() : el))
+        .join("");
+
+      const firstLetter = name.charCodeAt(0) - 65;
+
+      const currentContent = liElements[firstLetter].innerText;
+
+      liElements[firstLetter].innerText = !currentContent
+        ? name
+        : currentContent + `, ${name}`;
+      inputEl.value = "";
+    },
+  };
+
+  const inputEl = document.querySelector('input[type="text"]');
+  const olEl = document.querySelector('ol[type="A"]');
+
+  if (inputEl === null || olEl === null) {
+    throw new Error("Something happens please reload the page!");
+  }
+
+  document.getElementsByTagName("button")[0].addEventListener("click", (e) => {
+    if (typeof actions[e.target.getAttribute("type")] === "function") {
+      actions[e.target.getAttribute("type")]();
+    }
+  });
 }
--- JavaScriptDom/Exercises/06. Table-Search-Engine/solution.js
+++ JavaScriptDom/Exercises/06. Table-Search-Engine/solution.js
@@ -1,3 +1,28 @@
 function solve() {
-   //TODO...
+  const tdElements = document.querySelectorAll("tbody > tr");
+
+  const actions = {
+    searchBtn: () => {
+      const inputEl = document.querySelector("#searchField");
+
+      if (inputEl === null) {
+        throw new Error("Something happens please reload the page!");
+      }
+      
+      const searchedText = inputEl.value;
+      inputEl.value = "";
+
+      for (let i = 0; i < tdElements.length; i++) {
+        searchedText && tdElements[i].innerText.includes(searchedText)
+          ? tdElements[i].classList.add("select")
+          : tdElements[i].classList.remove("select");
+      }
+    },
+  };
+
+  document.addEventListener("click", (e) => {
+    if (typeof actions[e.target.id] === "function") {
+      actions[e.target.id]();
+    }
+  });
 }
