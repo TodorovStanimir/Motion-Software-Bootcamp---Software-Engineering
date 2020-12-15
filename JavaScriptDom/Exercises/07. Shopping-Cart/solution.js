e1d98212246c28c2c0290dc449455baee8f9fbe5
TodorovStanimir <52385215+TodorovStanimir@users.noreply.github.com>
2020-12-15 19:48:22 +0200

    feat: solved Shopping-Cart

--- JavaScriptDom/Exercises/07. Shopping-Cart/solution.js
+++ JavaScriptDom/Exercises/07. Shopping-Cart/solution.js
@@ -1,3 +1,46 @@
 function solve() {
-   //TODO...
+  let totalAmount = 0;
+  const bouthProducts = [];
+  const actions = {
+    "add-product": (e) => {
+      const parentEl = e.target.parentNode.parentNode;
+      const resultEl = document.querySelector("textarea");
+      if (parentEl === null || resultEl === null) {
+        throw new Error("Something happens please reload the page!");
+      }
+      const priceEl = parentEl.querySelector(".product-line-price");
+      const nameEl = parentEl.querySelector(".product-title");
+
+      if (priceEl === null || nameEl === null) {
+        throw new Error("Something happens please reload the page!");
+      }
+      const price = +priceEl.innerText.match(/(\d*\.?\d+)/gi)[0];
+      const name = nameEl.innerText;
+      !bouthProducts.includes(name) ? bouthProducts.push(name) : null;
+      totalAmount += price;
+
+      resultEl.innerHTML += resultEl.innerHTML
+        ? `\nAdded ${name} for ${price.toFixed(2)} to the cart.`
+        : `Added ${name} for ${price.toFixed(2)} to the cart.`;
+    },
+    checkout: (e) => {
+      const resultEl = document.querySelector("textarea");
+      if (resultEl === null) {
+        throw new Error("Something happens please reload the page!");
+      }
+      resultEl.innerHTML += `\nYou bought ${bouthProducts.join(
+        ", "
+      )} for ${totalAmount.toFixed(2)}.`;
+
+      const buttons = Array.from(document.querySelectorAll("button")).map(
+        (btn) => (btn.disabled = true)
+      );
+    },
+  };
+
+  document.addEventListener("click", function (e) {
+    if (typeof actions[e.target.getAttribute("class")] === "function") {
+      actions[e.target.getAttribute("class")](e);
+    }
+  });
 }
