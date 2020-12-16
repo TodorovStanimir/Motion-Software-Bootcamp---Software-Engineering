bacee7f98fec6448a96159d4419a2cfed8e317a1
TodorovStanimir <52385215+TodorovStanimir@users.noreply.github.com>
2020-12-16 23:44:42 +0200

    feat: solved List-Of-Items task

--- JavaScriptDomManipulation/Lab/01. List-Of-Items/app.js
+++ JavaScriptDomManipulation/Lab/01. List-Of-Items/app.js
@@ -1,3 +1,47 @@
-function addItem() {
-    console.log('TODO:...');
+function createElement(type, content) {
+  let e = document.createElement(type);
+
+  if (typeof content === "string") {
+    e.innerHTML = content;
+  }
+
+  if (typeof content === "object") {
+    e.appendChild(content);
+  }
+
+  return e;
 }
+function clickHandler(output, elementCreator, input) {
+  addItem(output, elementCreator, input.value);
+}
+
+function addToHTML(parent, child) {
+  parent.appendChild(child);
+}
+
+function addItem(output, elementCreator, value) {
+  if (value !== "") {
+    output(elementCreator(value));
+  }
+}
+
+function main() {
+  const cta = document.getElementById("addCTA");
+  const list = document.getElementById("items");
+  const input = document.getElementById("newItemText");
+
+  if (cta === null || list === null || input === null) {
+    throw new Error("Missing dom elements");
+  }
+
+  const boundClickHandler = clickHandler.bind(
+    undefined,
+    addToHTML.bind(undefined, list),
+    createElement.bind(undefined, "li"),
+    input
+  );
+
+  cta.addEventListener("click", boundClickHandler);
+}
+
+document.addEventListener("DOMContentLoaded", main);
