e2c8cb311a10d6c100152c7338a5481c7f08b7fa
TodorovStanimir <52385215+TodorovStanimir@users.noreply.github.com>
2020-12-17 23:35:33 +0200

    refactor: Delete-From-Table task

--- JavaScriptDomManipulation/Lab/03. Delete-From-Table/app.js
+++ JavaScriptDomManipulation/Lab/03. Delete-From-Table/app.js
@@ -1,23 +1,48 @@
-function deleteByEmail() {
+function deleteRowByEmail(callback, output, rows, searchedText) {
+  return output(callback(rows, searchedText));
+}
+
+function addInnerText(htmlEl, boolean) {
+  htmlEl.innerHTML = boolean ? "Deleted." : "Not found.";
+}
+
+function findHTMLElementByInnerText(HTMLCollection, searchedText) {
+  return [...HTMLCollection].find((row) => row.innerText.includes(searchedText));
+}
+
+function removeHTMLElementItself(htmlElement) {
+  if (htmlElement) {
+    htmlElement.remove();
+    return true;
+  }
+  return false;
+}
+
+function main() {
   const inputEl = document.querySelector("input[name=email]");
   const resultEl = document.querySelector("#result");
+  const ctaDelete = document.querySelector("#ctaDelete");
+  const rows = document.getElementsByTagName("tr");
 
-  if (inputEl === null || resultEl === null) {
+  if (inputEl === null || resultEl === null || ctaDelete === null) {
     throw new Error("Missing HTML elements. Please reload the page");
   }
 
-  const roows = document.querySelectorAll("tr");
-  const searchedText = inputEl.value;
-  let notDeletedRow = true;
-
-  if (searchedText) {
-    [...roows].map((row) => {
-      if (row.innerText.match(searchedText)) {
-        row.remove();
-        notDeletedRow = false;
-      }
-    });
+  function deleteRowHandler() {
+    if (inputEl.value) {
+      addInnerText(
+        resultEl,
+        deleteRowByEmail(
+          findHTMLElementByInnerText,
+          removeHTMLElementItself,
+          rows,
+          inputEl.value
+        )
+      );
+    }
   }
 
-  resultEl.innerHTML = notDeletedRow ? "Not found." : "Deleted.";
+  ctaDelete.addEventListener("click", deleteRowHandler);
 }
+
+document.addEventListener("DOMContentLoaded", main);
