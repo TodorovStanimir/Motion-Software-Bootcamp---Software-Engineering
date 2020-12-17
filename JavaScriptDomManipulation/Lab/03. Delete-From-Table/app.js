1dbfe77d260f470cc1eb84719878a51a36fcd62c
TodorovStanimir <52385215+TodorovStanimir@users.noreply.github.com>
2020-12-17 16:10:34 +0200

    feat: solved Delete-From-Table task

--- JavaScriptDomManipulation/Lab/03. Delete-From-Table/app.js
+++ JavaScriptDomManipulation/Lab/03. Delete-From-Table/app.js
@@ -1,3 +1,23 @@
 function deleteByEmail() {
-    console.log('TODO:...');
+  const inputEl = document.querySelector("input[name=email]");
+  const resultEl = document.querySelector("#result");
+
+  if (inputEl === null || resultEl === null) {
+    throw new Error("Missing HTML elements. Please reload the page");
+  }
+
+  const roows = document.querySelectorAll("tr");
+  const searchedText = inputEl.value;
+  let notDeletedRow = true;
+
+  if (searchedText) {
+    [...roows].map((row) => {
+      if (row.innerText.match(searchedText)) {
+        row.remove();
+        notDeletedRow = false;
+      }
+    });
+  }
+
+  resultEl.innerHTML = notDeletedRow ? "Not found." : "Deleted.";
 }
