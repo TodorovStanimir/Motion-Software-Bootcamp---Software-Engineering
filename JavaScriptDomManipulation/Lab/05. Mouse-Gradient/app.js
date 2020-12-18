4db8cbc53d69fbec705cdd8197160f7c13e89217
TodorovStanimir <52385215+TodorovStanimir@users.noreply.github.com>
2020-12-19 01:02:22 +0200

    feat: solved Mouse-Gradient Task

--- JavaScriptDomManipulation/Lab/05. Mouse-Gradient/app.js
+++ JavaScriptDomManipulation/Lab/05. Mouse-Gradient/app.js
@@ -1,3 +1,28 @@
 function attachGradientEvents() {
-    console.log('TODO:...');
+  const resultEl = document.getElementById("result");
+  const gradientEl = document.getElementById("gradient");
+
+  if (resultEl === null || gradientEl === null) {
+    throw new Error("Missing HTML elements.");
+  }
+
+  function calculatePower(e) {
+    return Math.floor((e.offsetX / (e.target.clientWidth - 1)) * 100) + "%";
+  }
+
+  function addTextContent(htmlEl, content) {
+    htmlEl.textContent = content;
+  }
+
+  function gradientMouseMoveHandler(e) {
+    addTextContent(resultEl, calculatePower(e));
+  }
+
+  function gradientMouseOutHandler(e) {
+    addTextContent(resultEl, "");
+  }
+
+  gradientEl.addEventListener("mousemove", gradientMouseMoveHandler);
+
+  gradientEl.addEventListener("mouseout", gradientMouseOutHandler);
 }
