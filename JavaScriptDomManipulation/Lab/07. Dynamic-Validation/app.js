821b3d7cd784478ffde2c4927c937eb2df61b93f
TodorovStanimir <52385215+TodorovStanimir@users.noreply.github.com>
2020-12-19 10:49:17 +0200

    feat: solved Dynamic-Validation task

--- JavaScriptDomManipulation/Lab/07. Dynamic-Validation/app.js
+++ JavaScriptDomManipulation/Lab/07. Dynamic-Validation/app.js
@@ -1,3 +1,29 @@
 function validate() {
-    console.log('TODO:...');
+  const inputEl = document.querySelector("#email");
+
+  if (inputEl === null) {
+    throw new Error("Missing input element");
+  }
+
+  function addClass(el, newClass) {
+    el.classList.add(newClass);
+  }
+  function removeClass(el, redundantClass) {
+    el.classList.remove(redundantClass);
+  }
+  function checkEmail(string) {
+    const emailPattern = /(.+)@(.+){2,}\.(.+){2,}/i;
+    return emailPattern.test(string);
+  }
+  function validateEmail(string, checkEmail, removeClass, addClass) {
+    checkEmail(string)
+      ? removeClass(inputEl, "error")
+      : addClass(inputEl, "error");
+  }
+
+  function validateEmailHandler(e) {
+    validateEmail(e.target.value, checkEmail, removeClass, addClass);
+  }
+
+  inputEl.addEventListener("change", validateEmailHandler);
 }
