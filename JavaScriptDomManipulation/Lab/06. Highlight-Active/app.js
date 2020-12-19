dea3b2042a91270ea03e2f44b8dbceb3ffef8827
TodorovStanimir <52385215+TodorovStanimir@users.noreply.github.com>
2020-12-19 10:10:49 +0200

    feat: solved Highlight-Active task

--- JavaScriptDomManipulation/Lab/06. Highlight-Active/app.js
+++ JavaScriptDomManipulation/Lab/06. Highlight-Active/app.js
@@ -1,3 +1,30 @@
 function focus() {
-    console.log('TODO:...');
+  const inputEl = document.querySelectorAll("input[type=text]");
+
+  function addClass(htmlEl, newClass) {
+    htmlEl.classList.add(newClass);
+  }
+
+  function removeClass(htmlEl, redundantClass) {
+    htmlEl.classList.remove(redundantClass);
+  }
+
+  function getParentEl(htmlEl) {
+    return htmlEl.parentNode;
+  }
+
+  function executeOnInputEl(nodeName, f) {
+    if (nodeName === "INPUT") f();
+  }
+
+  function focusHandler(e) {
+    executeOnInputEl(e.target, addClass(getParentEl(e.target), "focused"));
+  }
+
+  function blurHandler(e) {
+    executeOnInputEl(e.target, removeClass(getParentEl(e.target), "focused"));
+  }
+  
+  document.addEventListener("focus", focusHandler, true);
+  document.addEventListener("blur", blurHandler, true);
 }
