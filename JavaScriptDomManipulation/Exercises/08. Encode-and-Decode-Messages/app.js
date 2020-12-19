a684b76fbfdd6510b654be312332983bdcdcb7e2
TodorovStanimir <52385215+TodorovStanimir@users.noreply.github.com>
2020-12-20 01:29:07 +0200

    feat: solved Encode-and-Decode-Messages task

--- JavaScriptDomManipulation/Exercises/08. Encode-and-Decode-Messages/app.js
+++ JavaScriptDomManipulation/Exercises/08. Encode-and-Decode-Messages/app.js
@@ -1,3 +1,27 @@
 function encodeAndDecodeMessages() {
-    console.log('TODO...')
+  const [senderTextEl, receiverTextEl] = document.getElementsByTagName(
+    "textarea"
+  );
+
+  actions = {
+    "Decode and read it": function () {
+      receiverTextEl.value = receiverTextEl.value
+        .split("")
+        .map((el) => String.fromCharCode(el.charCodeAt() - 1))
+        .join("");
+    },
+    "Encode and send it": function () {
+      receiverTextEl.value = senderTextEl.value
+        .split("")
+        .map((el) => String.fromCharCode(el.charCodeAt() + 1))
+        .join("");
+      senderTextEl.value = "";
+    },
+  };
+
+  document.addEventListener("click", function (e) {
+    if (typeof actions[e.target.textContent] === "function") {
+      actions[e.target.textContent]();
+    }
+  });
 }
