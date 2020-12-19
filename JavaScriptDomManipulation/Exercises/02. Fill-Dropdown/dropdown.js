2d192a47b3a2ee79d1b797bfc9c547b411e1ce14
TodorovStanimir <52385215+TodorovStanimir@users.noreply.github.com>
2020-12-19 13:53:59 +0200

    feat: solved Fill-Dropdown task

--- JavaScriptDomManipulation/Exercises/02. Fill-Dropdown/dropdown.js
+++ JavaScriptDomManipulation/Exercises/02. Fill-Dropdown/dropdown.js
@@ -1,3 +1,54 @@
-function addItem() {
-    console.log('TODO:...');
+function createElement(type, content, value) {
+  let e = document.createElement(type);
+  e.value = value;
+
+  if (typeof content === "string") {
+    e.innerHTML = content;
+  }
+
+  if (typeof content === "object") {
+    e.appendChild(content);
+  }
+
+  return e;
 }
+
+function getValue(el) { return el.value; }
+
+function clearValue(el) {return el.value = ""; }
+
+function addToHTML(parent, child) { parent.appendChild(child); }
+
+function addItem(output, elementCreator, content, value) {
+  if (content !== "" && value !== "") {
+    output(elementCreator(content, value));
+  }
+}
+
+function main() {
+  const newItemTextEl = document.querySelector("#newItemText");
+  const newItemValueEl = document.querySelector("#newItemValue");
+  const menuEl = document.querySelector("#menu");
+  const ctaAdd = document.querySelector("input[value=Add]");
+
+  if (newItemTextEl === null, newItemValueEl === null, menuEl === null, ctaAdd === null) {
+    throw new Error("Missing html elements.");
+  }
+
+  function addItemHandler() {
+    addItem(
+      addToHTML.bind(undefined, menuEl),
+      createElement.bind(undefined, "option"),
+      getValue(newItemTextEl),
+      getValue(newItemValueEl)
+    );
+    clearValue(newItemTextEl);
+    clearValue(newItemValueEl);
+  }
+
+  const boundAddItemHandler = addItemHandler.bind(undefined);
+
+  ctaAdd.addEventListener("click", boundAddItemHandler);
+}
+
+document.addEventListener("DOMContentLoaded", main);
