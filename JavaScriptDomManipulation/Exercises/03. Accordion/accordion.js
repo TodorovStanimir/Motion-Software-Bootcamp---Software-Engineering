08e69036767047482a6f17b02194a233edf57f63
TodorovStanimir <52385215+TodorovStanimir@users.noreply.github.com>
2020-12-19 18:50:27 +0200

    feat: solved Accordion task

--- JavaScriptDomManipulation/Exercises/03. Accordion/accordion.js
+++ JavaScriptDomManipulation/Exercises/03. Accordion/accordion.js
@@ -1,3 +1,34 @@
-function toggle() {
-    console.log('TODO:...');
+function changeStyleProp(display, el) {
+  el.style.display = display;
 }
+
+function changeTextContent(content, el) {
+  el.textContent = content;
+}
+
+function getTextContent(el) {
+  return el.textContent;
+}
+
+function toggle(styleChanger, textContentChanger, extraEl, ctaButton) {
+  if (getTextContent(ctaButton) === "More") {
+    styleChanger("block", extraEl);
+    textContentChanger("Less", ctaButton);
+  } else {
+    styleChanger("none", extraEl);
+    textContentChanger("More", ctaButton);
+  }
+}
+
+function main() {
+  const extraEl = document.querySelector("#extra");
+  const ctaButton = document.querySelector("span[class=button]");
+
+  function toggleHandle() {
+    toggle(changeStyleProp, changeTextContent, extraEl, ctaButton);
+  }
+
+  ctaButton.addEventListener("click", toggleHandle);
+}
+
+document.addEventListener("DOMContentLoaded", main);
