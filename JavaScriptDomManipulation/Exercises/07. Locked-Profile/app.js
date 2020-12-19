2438ac643a8f7b6ed186660d8d8d64e243bd80e2
TodorovStanimir <52385215+TodorovStanimir@users.noreply.github.com>
2020-12-20 01:06:00 +0200

    feat: solved Locked-Profile task

--- JavaScriptDomManipulation/Exercises/07. Locked-Profile/app.js
+++ JavaScriptDomManipulation/Exercises/07. Locked-Profile/app.js
@@ -1,3 +1,35 @@
 function lockedProfile() {
-    console.log('TODO...')
+  function showAndHideProfile(message, displayProp, el) {
+    if (!checkIsLocked(el)) {
+      changeTextContent(
+        message,
+        el,
+        toggleStyleDisplay(displayProp, findPreviouseSibling(el))
+      );
+    }
+  }
+  function findPreviouseSibling(el) { return el.previousElementSibling; }
+
+  function toggleStyleDisplay(string, el) { el.style.display = string; }
+
+  function changeTextContent(content, el) { el.textContent = content; }
+
+  function findParrentEl(el) { return el.parentElement; }
+
+  function checkIsLocked(el) { return findParrentEl(el).querySelector("input[value=lock]").checked; }
+
+  const actions = {
+    "Show more": function (el) {
+      showAndHideProfile("Hide it", "block", el);
+    },
+    "Hide it": function (el) {
+      showAndHideProfile("Show more", "none", el);
+    },
+  };
+
+  document.addEventListener("click", function (e) {
+    if (typeof actions[e.target.textContent] === "function") {
+      actions[e.target.textContent](e.target);
+    }
+  });
 }
