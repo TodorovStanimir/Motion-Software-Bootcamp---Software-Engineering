7fbf52a02ba73ceb09d9aff1df6e75624af551bb
TodorovStanimir <52385215+TodorovStanimir@users.noreply.github.com>
2020-12-19 12:47:01 +0200

    feat: solved Substraction task

--- JavaScriptDomManipulation/Exercises/01. Subtraction/subtract.js
+++ JavaScriptDomManipulation/Exercises/01. Subtraction/subtract.js
@@ -1,3 +1,32 @@
 function subtract() {
-    console.log('TODO:...');
+  const firstNumberEl = document.querySelector("#firstNumber");
+  const secondNumberEl = document.querySelector("#secondNumber");
+  const resultEl = document.querySelector("#result");
+
+  if (firstNumberEl === null || secondNumberEl === null || resultEl === null) {
+    throw new Error("Missing HTML elements.");
+  }
+  firstNumberEl.disabled = false;
+  secondNumberEl.disabled = false;
+
+  function getValueFromHTMLEl(htmlEl) { return htmlEl.value; }
+
+  function calculateValue(firstNumber = 0, secondNumber = 0) { return firstNumber - secondNumber; }
+
+  function addTextContentToHTMLEl(htmlEl, value) { htmlEl.textContent = value;
+  }
+
+  function recalculateHandler() {
+    addTextContentToHTMLEl(
+      resultEl,
+      calculateValue(
+        Number(getValueFromHTMLEl(firstNumberEl)),
+        Number(getValueFromHTMLEl(secondNumberEl))
+      )
+    );
+  }
+
+  recalculateHandler();
+
+  document.addEventListener("change", recalculateHandler);
 }
