a1d711b289cd6aeff63a6709336c5d8ffd19e5c6
TodorovStanimir <52385215+TodorovStanimir@users.noreply.github.com>
2020-12-19 22:04:34 +0200

    feat: solved Notification task

--- JavaScriptDomManipulation/Exercises/05. Notification/scripts.js
+++ JavaScriptDomManipulation/Exercises/05. Notification/scripts.js
@@ -1,3 +1,23 @@
-function notify(message) {
-    console.log('TODO:...');
+function main() {
+  const notificationEl = document.querySelector("#notification");
+  const ctaShowNotification = document.querySelector("button");
+
+  if (notificationEl === null || ctaShowNotification === null) {
+    throw new Error("Missing notofication Element");
+  }
+
+  function notify(message) {
+    notificationEl.textContent = message;
+    notificationEl.style.display = "block";
+
+    setTimeout(() => {
+      notificationEl.style.display = "none";
+    }, 2000);
+  }
+
+  const boundNotify = notify.bind(undefined, "Something happened!")
+
+  ctaShowNotification.addEventListener("click", boundNotify);
 }
+
+document.addEventListener("DOMContentLoaded", main);
