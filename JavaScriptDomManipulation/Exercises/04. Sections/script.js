0c897224b44ed17be5765cd926029502b3b74d22
TodorovStanimir <52385215+TodorovStanimir@users.noreply.github.com>
2020-12-19 21:30:38 +0200

    feat: solved Sections task

--- JavaScriptDomManipulation/Exercises/04. Sections/script.js
+++ JavaScriptDomManipulation/Exercises/04. Sections/script.js
@@ -1,3 +1,44 @@
-function create(words) {
-   console.log('TODO:...');
+function createHTMLEl(type, content) {
+  let e = document.createElement(type);
+
+  if (typeof content === "string") {
+    e.innerHTML = content;
+  }
+
+  if (typeof content === "object") {
+    e.appendChild(content);
+  }
+
+  return e;
 }
+function changeStyleOfHTMLEl(htmlEl, property, value) {
+  return (htmlEl.style[`${property}`] = value);
+}
+function addToHTML(parent, child) {
+  parent.appendChild(child);
+}
+function addEventListener(htmlEl, event, func) {
+  htmlEl.addEventListener(event, func);
+}
+function displayShowHandler(e) {
+  e.target.children[0].style.display = "block";
+}
+
+function create() {
+  const contentEl = document.querySelector("#content");
+
+  if (contentEl === null) {
+    throw new Error("Missing content element");
+  }
+  
+  ["Section 1", "Section 2", "Section 3", "Section 4"].map((word) => {
+    const newDiv = createHTMLEl("div");
+    const newPEl = createHTMLEl("p", word);
+    changeStyleOfHTMLEl(newPEl, "display", "none");
+    addEventListener(newDiv, "click", displayShowHandler);
+    addToHTML(newDiv, newPEl);
+    addToHTML(contentEl, newDiv);
+  });
+}
+
+document.addEventListener("DOMContentLoaded", create);
