93d15514e3da53e3c7e52906643abee3a41fbaf5
TodorovStanimir <52385215+TodorovStanimir@users.noreply.github.com>
2020-12-20 21:10:11 +0200

    feat: solved XHR task

--- JavascriptRESTservices&ajax/Lab/04.XHR/scripts/app.js
+++ JavascriptRESTservices&ajax/Lab/04.XHR/scripts/app.js
@@ -1,3 +1,17 @@
 function loadRepos() {
-   console.log("TODO...");
+  const url = "https://api.github.com/users/testnakov/repos";
+  const httpRequest = new XMLHttpRequest();
+  const resultEl = document.querySelector("#res");
+
+  if (resultEl === null) {
+    throw new Error("Missing a result element");
+  }
+  httpRequest.open("GET", url);
+  httpRequest.send();
+
+  httpRequest.addEventListener("readystatechange", function () {
+    if (httpRequest.readyState == 4 && httpRequest.status == 200) {
+      document.querySelector("#res").textContent = httpRequest.responseText;
+    }
+  });
 }
