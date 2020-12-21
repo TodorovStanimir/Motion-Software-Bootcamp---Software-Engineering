3b6c7a172bd128f4b763a83e83a70f68cd20a11c
TodorovStanimir <52385215+TodorovStanimir@users.noreply.github.com>
2020-12-21 15:44:54 +0200

    feat: solved MESSENGER task

--- JavascriptRESTservices&ajax/Exercises/04.MESSENGER/app.js
+++ JavascriptRESTservices&ajax/Exercises/04.MESSENGER/app.js
@@ -1,5 +1,59 @@
 function attachEvents() {
-    console.log('TODO...');
+  const url = "https://rest-messanger.firebaseio.com/messanger";
+  const [authorEl, contentEl] = document.querySelectorAll("input");
+  const messages = document.querySelector("#messages");
+
+  if (authorEl === null || contentEl === null || messages === null) {
+    throw new Error("Missing html elements.");
+  }
+
+  function handleError(res) {
+    if (!res.ok) {
+      throw new Error("Error");
+    }
+    return res.json();
+  }
+
+  const actions = {
+    Send: function () {
+      const [author, content] = [authorEl.value, contentEl.value];
+
+      if (!author || !content) {
+        return;
+      }
+
+      fetch(`${url}.json`, {
+        method: "POST",
+        body: JSON.stringify({ author, content }),
+      })
+        .then(handleError)
+        .then(() => {
+          messages.value += `\n${author}: ${content}`;
+          messages.scrollTop = messages.scrollHeight;
+          authorEl.value = "";
+          contentEl.value = "";
+        })
+        .catch((err) => console.log(err));
+    },
+    Refresh: function () {
+      fetch(`${url}.json`)
+        .then(handleError)
+        .then((mes) => {
+          Object.values(mes).map(({ author, content }) => {
+            messages.value += `${author}: ${content}\n`;
+          });
+          messages.value = messages.value.trim()
+          messages.scrollTop = messages.scrollHeight;
+        })
+        .catch((err) => console.log(err));
+    },
+  };
+
+  document.addEventListener("click", function (e) {
+    if (typeof actions[e.target.value] === "function") {
+      actions[e.target.value]();
+    }
+  });
 }
 
 attachEvents();
