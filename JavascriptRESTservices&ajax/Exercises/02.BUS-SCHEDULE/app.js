1aeea50ee2e26015170dd6c52954d369a6bb26c6
TodorovStanimir <52385215+TodorovStanimir@users.noreply.github.com>
2020-12-21 11:26:59 +0200

    solved BUS-SCHEDULE task

--- JavascriptRESTservices&ajax/Exercises/02.BUS-SCHEDULE/app.js
+++ JavascriptRESTservices&ajax/Exercises/02.BUS-SCHEDULE/app.js
@@ -1,17 +1,56 @@
 function solve() {
+  const infoEl = document.querySelector("#info");
+  const depCTA = document.querySelector("#depart");
+  const arrCTA = document.querySelector("#arrive");
 
-    function depart() {
-        console.log('Depart TODO...');
+  if (infoEl === null || depCTA === null || arrCTA === null) {
+    throw new Error("Missing html element/s");
+  }
+
+  let state = {
+    name: "",
+    next: "depot",
+  };
+
+  function toggleDisabled(el) {
+    Array.isArray(el)
+      ? el.map((el) => (el.disabled = !el.disabled))
+      : (el.disabled = !el.disabled);
+  }
+
+  function setTextContent(el, content) {
+    el.textContent = content;
+  }
+
+  function handleError(res) {
+    if (!res.ok) {
+      throw new Error("Error");
     }
+    return res.json();
+  }
 
-    function arrive() {
-        console.log('Arrive TODO...');
-    }
+  function depart() {
+    const url = `https://judgetests.firebaseio.com/schedule/${state.next}.json`;
 
-    return {
-        depart,
-        arrive
-    };
+    fetch(url)
+      .then(handleError)
+      .then((data) => {
+        state = { ...state, ...data };
+        setTextContent(infoEl, `Next stop ${state.name}`);
+        toggleDisabled([depCTA, arrCTA]);
+      })
+      .catch((err) => (infoEl.textContent = err.message));
+  }
+
+  function arrive() {
+    setTextContent(infoEl, `Arriving at ${state.name}`);
+    toggleDisabled([depCTA, arrCTA]);
+  }
+
+  return {
+    depart,
+    arrive,
+  };
 }
 
 let result = solve();
